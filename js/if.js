e7b75e509ab26ffdceefbc5aeeb046349ab932b7
gboned <gboned@cifpfbmoll.eu>
2020-05-16 16:21:48 +0200

    Añadir prompt para indicar opción de Jugador 1

--- js/if.js
+++ js/if.js
@@ -5,12 +5,16 @@
  */
 
 console.log("Jugaremos a 'Piedra, Papel, Tijera' al mejor de tres.");
+console.log("Cada jugador debe introducir su opción; piedra, papel o tijera. Pueden comenzar tanto con minúscula como con mayúscula.")
 
 function juego() {
-    return opcionJugador1("patata");
+    return opcionJugador1();
 }
 
-function opcionJugador1(opcion) {
+function opcionJugador1() {
+
+    var opcion = window.prompt("Selecciona la opción del Jugador 1");
+
     if (opcion === "Piedra" || opcion == "Papel" || opcion == "Tijera") {
         return opcion;
     }
