539a4fff03f295098bada20676652f0484c2a9c4
gboned <gboned@cifpfbmoll.eu>
2020-05-16 17:42:28 +0200

    Añadir función de la opción del jugador 1

--- js/switch.js
+++ js/switch.js
@@ -16,3 +16,25 @@ function juegoSwitch() {
     console.log("El Jugador 1 ha elegido " + opcionJugador1() + ", y el Jugador 2 ha elegido " + opcionJugador2());
     return ganador();
 }
+
+// Creo la función que pida al usuario introducir la opción del primer jugador.
+function opcionJugador1() {
+
+    var opcion = window.prompt("Selecciona la opción del Jugador 1");
+    opcionJ1 = opcion;
+
+    switch (opcion) {
+        case 'Piedra':
+            console.log(opcion);
+            break;
+        case 'Papel':
+            console.log(opcion);
+            break;
+        case 'Tijera':
+            console.log(opcion);
+            break;
+        default:
+            console.log("La opción escogida no existe. Por favor, introduce Piedra, Papel o Tijera (Recuerda que deben empezar por mayúscula).")
+            opcionJugador1();
+    }
+}
