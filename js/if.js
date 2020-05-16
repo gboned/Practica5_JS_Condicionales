26141004e19eeb6cacc8318e293ff76ce348d178
gboned <gboned@cifpfbmoll.eu>
2020-05-16 17:00:41 +0200

    Añadir variable global

--- js/if.js
+++ js/if.js
@@ -7,15 +7,20 @@
 console.log("Jugaremos a 'Piedra, Papel, Tijera' al mejor de tres.");
 console.log("Cada jugador debe introducir su opción; piedra, papel o tijera. Pueden comenzar tanto con minúscula como con mayúscula.")
 
+var opcionJ1;
+var opcionJ2;
+
 // Creo la función que inicializará el juego.
 function juego() {
-    return "El Jugador 1 ha elegido " + opcionJugador1() + ", y el Jugador 2 ha elegido " + opcionJugador2();
+    console.log("El Jugador 1 ha elegido " + opcionJugador1() + ", y el Jugador 2 ha elegido " + opcionJugador2());
+    return ganador();
 }
 
 // Creo la función que pida al usuario introducir la opción del primer jugador.
 function opcionJugador1() {
 
     var opcion = window.prompt("Selecciona la opción del Jugador 1");
+    opcionJ1 = opcion;
 
     if (opcion === "Piedra" || opcion == "Papel" || opcion == "Tijera") {
         return opcion;
@@ -34,6 +39,7 @@ function opcionJugador1() {
 function opcionJugador2() {
 
     var opcion = window.prompt("Selecciona la opción del Jugador 2");
+    opcionJ2 = opcion;
 
     if (opcion === "Piedra" || opcion == "Papel" || opcion == "Tijera") {
         return opcion;
@@ -45,5 +51,13 @@ function opcionJugador2() {
 
     else {
         console.log("La opción escogida no existe. Por favor, introduce piedra, papel o tijera.");
+    }
+}
+
+// Creo la función que devuelve cuál es el ganador de la ronda.
+function ganador() {
+
+    if ( opcionJ1 === "Piedra" && opcionJ2 === "Papel") {
+        console.log("El ganador es Jugador 2 porque " + opcionJ2 + " gana a " + opcionJ1);
     }
 }
