4894d7045b9c5258889476ecce9330a6cb6ecf15
gboned <gboned@cifpfbmoll.eu>
2020-05-16 18:12:45 +0200

    Añadir return a función de opcionJugador1

--- js/switch.js
+++ js/switch.js
@@ -25,14 +25,11 @@ function opcionJugador1() {
 
     switch (opcion) {
         case 'Piedra':
-            console.log(opcion);
-            break;
+            return opcion;
         case 'Papel':
-            console.log(opcion);
-            break;
+            return opcion;
         case 'Tijera':
-            console.log(opcion);
-            break;
+            return opcion;
         default:
             console.log("La opción escogida no existe. Por favor, introduce Piedra, Papel o Tijera (Recuerda que deben empezar por mayúscula).")
             opcionJugador1();
@@ -59,3 +56,29 @@ function opcionJugador2() {
             opcionJugador2();
     }
 }
+
+// Creo la función que devuelve cuál es el ganador de la ronda.
+function ganador() {
+
+    // Creo identificadores para cada opcion.
+    Pi = "Piedra";
+    Pa = "Papel";
+    Ti = "Tijera";
+
+    switch (opcionJ1 | opcionJ2) {
+        case opcionJ1.Pi | opcionJ2.Pa:
+        case opcionJ1.Pa | opcionJ2.Ti:
+        case opcionJ1.Ti | opcionJ2.Pi:
+            console.log("El ganador es Jugador 2 porque " + opcionJ2 + " gana a " + opcionJ1);
+            break;
+        case opcionJ2.Pi | opcionJ1.Pa:
+        case opcionJ2.Pa | opcionJ1.Ti:
+        case opcionJ2.Ti | opcionJ1.Pi:
+            console.log("El ganador es Jugador 1 porque " + opcionJ1 + " gana a " + opcionJ1);
+            break;  
+        default:
+            console.log("Ambos Jugadores han escogido la misma opción, por lo que hay un empate.")
+    }
+}
+
+juegoSwitch();
