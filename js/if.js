1b516e08ec0ce1b8d8ea098f6c46951e349ae37f
gboned <gboned@cifpfbmoll.eu>
2020-05-16 16:14:35 +0200

    Introducir funciones principales

--- js/if.js
+++ js/if.js
@@ -5,3 +5,21 @@
  */
 
 console.log("Jugaremos a 'Piedra, Papel, Tijera' al mejor de tres.")
+
+function juego() {
+    return opcionJugador1("patata");
+}
+
+function opcionJugador1(opcion) {
+    if (opcion === "Piedra" || opcion == "Papel" || opcion == "Tijera") {
+        return opcion;
+    }
+
+    else if (opcion === "piedra" || opcion == "papel" || opcion == "tijera") {
+        return opcion;
+    }
+
+    else {
+        console.log("La opción escogida no existe. Por favor, introduce piedra, papel o tijera".);
+    }
+}
